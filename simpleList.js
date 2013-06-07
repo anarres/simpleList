57dd729626699fa8e6e26619ef9dc0c3fbe9cf3e
anarres <info@foofurple.com>
2013-06-07 18:28:17 +0100

    vastly improved

--- simpleList.js
+++ simpleList.js
@@ -1,68 +1,76 @@
+function init() {
+//    localStorage.clear();
+    updateStorageInfo();
+}
+
 function updateStorageInfo() {
-    var html;
+    var html = "";
     if(typeof(Storage)==="undefined") {
         html = "<p>Sorry, your browser doesn't have local storage support, \
              so this app probably won't work for you.</p>";
     }
     else {
-        if (localStorage.length === 0) {
-            html = "<p>There are no things in the list yet. Why not use \
-                    the form below to add some?</p>";
-        }
-        else {
+        var foo = localStorage.getItem('simpleList');
+        if (foo) {
+            var bar = JSON.parse(foo);
             html = "<ul>";
-            for (var i=0; i<localStorage.length; i++) {
+            for (var i=0; i<bar.length; i++) {
                 var oneUp = i+1;
                 html += "<li>";
                 html += oneUp;
                 html += ": ";
-                html += localStorage[i];
+                html += bar[i]["thing"];
                 html += "<span class='remove' title='Remove' onclick='removeThing(";
                 html += i;
                 html += ")'> x</span>";
                 html += "</li>";
             }
+            html += "</ul>";
         }
-        html += "</ul>";
-        document.getElementById("storage").innerHTML = html;
+        else {
+            html += "There are no things saved yet, but you can use the form below to add some.";
+        }
     }
+    document.getElementById("storage").innerHTML = html;
 }
 
-function addThingToStorage(thing) {
-    var myIndex = localStorage.length;
-    localStorage[myIndex] = thing;
-    updateStorageInfo();
-}
 
 function saveAThing() {
     var thing = document.getElementById("newThingInput").value;
-    addThingToStorage(thing);
+    var when = new Date().getTime();
+    var obj = {"thing":thing, "when":when};
+
+    var foo = localStorage.getItem('simpleList');
+    if (foo) {
+        var listOfThings = JSON.parse(foo);
+        listOfThings.push(obj);
+        localStorage.setItem('simpleList', JSON.stringify(listOfThings));
+    }
+    else {
+        var listOfThings = [obj];
+        localStorage.setItem('simpleList', JSON.stringify(listOfThings));
+    }
     updateStorageInfo();
 }
 
 function removeThing(myIndex) {
-    var myArray = [];
-    for (var i=0; i<localStorage.length; i++) {
-        if (i < myIndex) {
-            myArray.push(localStorage[i]);
+    var foo = localStorage.getItem('simpleList');
+    if (foo) {
+        var listOfThings = JSON.parse(foo);
+        var newList = [];
+
+        for (var i=0; i<listOfThings.length; i++) {
+            if (i !== myIndex) {
+                newList.push(listOfThings[i]);
+            }
         }
-        else if (i > myIndex) {
-            myArray.push(localStorage[i]);
-        }
+        localStorage.setItem('simpleList', JSON.stringify(newList));
     }
-    localStorage.clear();
-    for (var j=0; j<myArray.length; j++) {
-        localStorage[j] = myArray[j];    
+    else {
+        console.log("Oh dear, that's not supposed to happen");
     }
     updateStorageInfo();
 }
-
-function init() {
-    updateStorageInfo();
-}
-
-
-
 
 
 
